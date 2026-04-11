a45d441b2a7997a3cc9f749cfd22a81d69e6dbe8
mohamed reda <ridabarhouma@gmail.com>
2026-04-11 17:16:16 +0200

    complete deployement

--- feedback-service/server.js
+++ feedback-service/server.js
@@ -2,10 +2,10 @@ const dotenv = require("dotenv");
 const path = require("path");
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const feedbackRoutes = require('./routes/feedbackRoutes');
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const feedbackRoutes = require("./routes/feedbackRoutes");
 
 const app = express();
 
@@ -14,10 +14,10 @@ app.use(express.json());
 
 connectDB();
 
-app.use('/', feedbackRoutes);
+app.use("/", feedbackRoutes);
 
 const PORT = process.env.PORT_FEEDBACK || 5007;
 
 app.listen(PORT, () => {
-    console.log(`Feedback Service running on port ${PORT}`);
+  console.log(`Feedback Service running on port ${PORT}`);
 });
